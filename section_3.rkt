58e32284b72bdfe16868f637eb4560d44c848c5f
Noah <nbtheduke@gmail.com>
2017-04-15 15:50:02 -0400

    Exercise 3.3.1

--- section_3.rkt
+++ section_3.rkt
@@ -66,3 +66,43 @@
   (+ attendee-ceiling
      (* attendee-increase-per-dollar
         (- ticket-ceiling ticket-price))))
+
+; Exercise 3.3.1
+; Conversion from Imperial to Metric
+
+(define cm-in-inch 2.54)
+(define inches-in-foot 12)
+(define feet-in-yard 3)
+(define yards-in-rod 5.5)
+(define rods-in-furlong 40)
+(define furlongs-in-mile 8)
+
+(define (inch-to-cm inches)
+  (* inches cm-in-inch))
+
+(define (foot-to-inch feet)
+  (* feet inches-in-foot))
+
+(define (yard-to-foot yards)
+  (* yards feet-in-yard))
+
+(define (rod-to-yard rods)
+  (* rods yards-in-rod))
+
+(define (furlong-to-rod furlongs)
+  (* furlongs rods-in-furlong))
+
+(define (mile-to-furlong miles)
+  (* miles furlongs-in-mile))
+
+(define (foot-to-cm feet)
+  (inch-to-cm (foot-to-inch feet)))
+
+(define (yard-to-cm yards)
+  (inch-to-cm (foot-to-inch (yard-to-foot yards))))
+
+(define (rod-to-inch rods)
+  (foot-to-inch (yard-to-foot (rod-to-yard rods))))
+
+(define (mile-to-feet miles)
+  (yard-to-foot (rod-to-yard (furlong-to-rod (mile-to-furlong miles)))))
